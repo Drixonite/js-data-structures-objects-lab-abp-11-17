484f4ee7a5a00a47d7d5e440adeab5e83f6ecbc7
David Kim <mail.ddk@gmail.com>
2017-11-14 05:11:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, name, address) {
 function deleteFromDriverByKey(driver, key) {
   const newDriver = {...driver};
   delete newDriver.key;
+  return newDriver;
 }
 
 function destructivelyDeleteFromdriverByKey(driver, key) {
