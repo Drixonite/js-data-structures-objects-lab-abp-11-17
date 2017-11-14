a6fa49ae1d42fb03282a2e7713b853d3a307c957
David Kim <mail.ddk@gmail.com>
2017-11-14 04:57:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function updateDriverWithKeyAndValue(driver, name, address) {
   newDriver[address] = '11 Broadway';
   return newDriver;
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, name, address) {
+  driver.assign({}, {[name]: Sam}, {[address]: 12 Broadway} );
+  return;
+}
